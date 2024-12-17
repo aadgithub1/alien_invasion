477b5954394613ab0b9bb90a1212716e85576491
aadgithub1 <152732836+aadgithub1@users.noreply.github.com>
2024-12-17 17:46:25 -0500

    Ship (server) can now move left and right.

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,12 +23,23 @@ class AlienInvasion():
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
         
     def run_game(self):
         """Runs the game."""
         #Continually check for events
         while True: #look at each event in even list
             self._check_events()
+            self.ship.update()
             self._update_screen()
             pygame.display.flip()
             self.clock.tick(60)
--- ship.py
+++ ship.py
@@ -5,13 +5,28 @@ class Ship():
     
     def __init__(self, alien_invasion_instance):
         """Initializes the ship."""
+        self.settings = alien_invasion_instance.settings
         self.screen = alien_invasion_instance.screen
         self.screen_rect = alien_invasion_instance.screen.get_rect()
         
-        self.image = pygame.image.load('server.png')
+        self.image = pygame.image.load('./alien_invasion/server.png')
         self.rect = self.image.get_rect()
-        
         self.rect.midbottom = self.screen_rect.midbottom
+        
+        self.actual_x = float(self.rect.x)
+        
+        self.moving_right = False
+        self.moving_left = False
+        
+    def update(self):
+        """Checks to see if the ship should be moving."""
+        if self.moving_right:
+            self.actual_x += self.settings.ship_speed
+        elif self.moving_left:
+            self.actual_x -= self.settings.ship_speed
+            
+        self.rect.x = self.actual_x
+        
         
     def blitme(self):
         """Draw ship at current location"""
